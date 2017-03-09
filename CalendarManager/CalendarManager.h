2ac875e8c7bd0e16385951b52c240ffa298c29bc
Mihovil <mihovil.kovacevic@shoutem.com>
2017-03-09 15:30:23 +0100

    Add dynamic header imports

--- CalendarManager/CalendarManager.h
+++ CalendarManager/CalendarManager.h
@@ -11,7 +11,16 @@
 
 
 #endif /* CalendarManager_h */
+
+// import RCTBridgeModule
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#elif __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
+#else
+#import "React/RCTBridgeModule.h"   // Required when used as a Pod in a Swift project
+#endif
+
 #import <EventKit/EventKit.h>
 #import <EventKitUI/EventKitUI.h>
 
